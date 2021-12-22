99219601fae0ca0659b38f2d2d5b740d5093a616
Anthony Davis <anthony.m.davis.26@gmail.com>
2021-12-22 05:21:16 -0800

    Implement ConnectionPool

---
+++ src/main/java/com/solvd/hospital/utils/ConnectionPool.java
@@ -0,0 +1,33 @@
+package com.solvd.hospital.utils;
+
+import java.util.concurrent.LinkedBlockingQueue;
+
+public class ConnectionPool {
+    private static ConnectionPool instance;
+    private static LinkedBlockingQueue<Connection> pool;
+    private static final int MAX_POOL_CAPACITY = 10;
+    private static int existingConnectionsCount = 0;
+
+    private ConnectionPool(){} // Prevent construction
+
+    public static ConnectionPool getInstance() {
+        if (instance == null)
+            instance = new ConnectionPool();
+
+        return instance;
+    }
+
+    public Connection getConnection() throws InterruptedException {
+        if (existingConnectionsCount < MAX_POOL_CAPACITY)
+        {
+            existingConnectionsCount++;
+            return new Connection();
+        }
+
+        return pool.take();
+    }
+
+    public void releaseConnection(Connection con) {
+        pool.add(con);
+    }
+}
